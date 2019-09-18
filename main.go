c58c1dfbf786302095a0ab5d971f2e46a5a13cc3
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2019-09-18 16:17:40 +0800

    fixup

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"sort"
 	"strings"
 	"time"
 
 	"github.com/google/go-github/v28/github"
 )
 
-func getAllVecExprPRs() []*github.PullRequest {
+func getAllVecExprPRs(begin, end time.Time) []*github.PullRequest {
 	client := github.NewClient(nil)
 	opt := &github.PullRequestListOptions{
 		State: "closed",
@@ -21,6 +22,8 @@ func getAllVecExprPRs() []*github.PullRequest {
 		panic(err)
 	}
 
+	resp.LastPage = 5
+
 	results := make([]*github.PullRequest, 0, 128)
 	for i := 0; i <= resp.LastPage; i++ {
 		fmt.Println("Total Page:", resp.LastPage, ", Page Now:", i)
@@ -30,19 +33,57 @@ func getAllVecExprPRs() []*github.PullRequest {
 			panic(err)
 		}
 		for _, pr := range prs {
-			if strings.Contains(*pr.Title, "implement vectorized evaluation") {
-				results = append(results, pr)
+			if !strings.Contains(*pr.Title, "implement vectorized evaluation") {
+				continue
 			}
+
+			closedAt := *pr.ClosedAt
+			if closedAt.Before(begin) || closedAt.After(end) {
+				continue
+			}
+
+			results = append(results, pr)
 		}
-		time.Sleep(time.Second)
+		time.Sleep(time.Second * 5) // avoid rate limit
 	}
 
 	return results
 }
 
 func main() {
-	prs := getAllVecExprPRs()
+	layout := "2006-01-02 15:04:05"
+	str := "2019-09-19 00:00:00"
+	end, err := time.Parse(layout, str)
+	if err != nil {
+		panic(err)
+	}
+	begin := end.Add(-time.Hour * 24 * 7)
+	fmt.Println("Begin:", begin, ", End:", end)
+
+	prs := getAllVecExprPRs(begin, end)
+	countMap := make(map[string][]*github.PullRequest)
 	for _, pr := range prs {
-		fmt.Println(*pr.Title)
+		user := *pr.User.Name
+		countMap[user] = append(countMap[user], pr)
+	}
+
+	type SortItem struct {
+		Cnt  int
+		User string
+	}
+	items := make([]*SortItem, 0, len(countMap))
+	for k, v := range countMap {
+		items = append(items, &SortItem{len(v), k})
+	}
+	sort.Slice(items, func(i, j int) bool {
+		return items[i].Cnt > items[j].Cnt
+	})
+
+	for _, it := range items {
+		fmt.Println(it.User, it.Cnt)
+		prs := countMap[it.User]
+		for _, pr := range prs {
+			fmt.Println(">>", *pr.Title)
+		}
 	}
 }
